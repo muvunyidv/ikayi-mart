39ada1c06654154d75149ababb5bc8e11a49059e
muvunyidv <muvunyiisaac202@gmail.com>
2026-08-18 13:38:09 +0200

    Deploy updated frontend code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2488051885" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1710443964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
